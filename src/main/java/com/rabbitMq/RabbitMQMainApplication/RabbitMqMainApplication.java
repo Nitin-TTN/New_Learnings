97967b487d116199006c678b474e6fc971df7322
Nitin-TTN <nitin1@tothenew.com>
2023-05-26 09:27:02 +0530

    small changes

--- src/main/java/com/rabbitMq/RabbitMQMainApplication/RabbitMqMainApplication.java
+++ src/main/java/com/rabbitMq/RabbitMQMainApplication/RabbitMqMainApplication.java
@@ -2,12 +2,11 @@ package com.rabbitMq.RabbitMQMainApplication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class RabbitMqMainApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(RabbitMqMainApplication.class, args);
 	}
-
 }
